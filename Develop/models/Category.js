414af1118667168afe5962efe5eed2d7728e29ae
Felix Francisco <Felixfrancisco1776@gmail.com>
2023-08-07 19:36:21 -0400

    first model update

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id:{
+      type; DataTransfer.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    }
+    category_name: {
+      YTPE: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
@@ -16,5 +25,105 @@ Category.init(
     modelName: 'category',
   }
 );
-
 module.exports = Category;
+
+
+// * `Category`
+
+//   * `id`
+
+//     * Integer.
+  
+//     * Doesn't allow null values.
+  
+//     * Set as primary key.
+  
+//     * Uses auto increment.
+
+//   * `category_name`
+  
+//     * String.
+  
+//     * Doesn't allow null values.
+
+// * `Product`
+
+//   * `id`
+  
+//     * Integer.
+  
+//     * Doesn't allow null values.
+  
+//     * Set as primary key.
+  
+//     * Uses auto increment.
+
+//   * `product_name`
+  
+//     * String.
+  
+//     * Doesn't allow null values.
+
+//   * `price`
+  
+//     * Decimal.
+  
+//     * Doesn't allow null values.
+  
+//     * Validates that the value is a decimal.
+
+//   * `stock`
+  
+//     * Integer.
+  
+//     * Doesn't allow null values.
+  
+//     * Set a default value of `10`.
+  
+//     * Validates that the value is numeric.
+
+//   * `category_id`
+  
+//     * Integer.
+  
+//     * References the `Category` model's `id`.
+
+// * `Tag`
+
+//   * `id`
+  
+//     * Integer.
+  
+//     * Doesn't allow null values.
+  
+//     * Set as primary key.
+  
+//     * Uses auto increment.
+
+//   * `tag_name`
+  
+//     * String.
+
+// * `ProductTag`
+
+//   * `id`
+
+//     * Integer.
+
+//     * Doesn't allow null values.
+
+//     * Set as primary key.
+
+//     * Uses auto increment.
+
+//   * `product_id`
+
+//     * Integer.
+
+//     * References the `Product` model's `id`.
+
+//   * `tag_id`
+
+//     * Integer.
+
+//     * References the `Tag` model's `id`.
